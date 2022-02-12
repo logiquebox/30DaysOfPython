2884caced94e4e92d761350c5afad956d3fb713b
Ujah Emmanuel <ujahemmanuel72@gmail.com>
2022-02-12 11:57:07 +0100

    Day4 commit

---
+++ workspace/Day4/iterations-and-loops.py
@@ -0,0 +1,15 @@
+# for loop
+customers = [{"name":"Emmanuel", "id":1}, {"name":"Blessing", "id":2}, 
+                {"name":"peter", "id":3}, {"name":"victor", "id":4}]
+for customer in customers:
+    print(customer['name'])
+
+
+numbers = [1,2,3,4,5,6,7,8,9]
+for i in numbers:
+    print(i)
+
+for x in range(0, 10):
+    print(f'the number is {x}')
+
+
